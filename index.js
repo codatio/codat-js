eee41dff9b50a454306d258d4fc6c47d0387baed
Jason Dryhurst-Smith <jason@codat.io>
2017-08-09 12:53:43 +0100

    fix the module load paths for the index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Main body of the client library
-exports.api = require('src/codat')
+exports.api = require('./src/codat.js')
 
 // Queries extension.
-exports.queries = require('src/codat-queries')
+exports.queries = require('./src/codat-queries.js')
