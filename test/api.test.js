19d53a2ad6a1bdf5fb451209f50de360537313fd
John Hicks <j.hicks@codat.io>
2019-09-05 14:12:30 +0100

    Unit tests for dataConnectionDataClient

--- test/api.test.js
+++ test/api.test.js
@@ -3,6 +3,8 @@ import should from 'should'
 
 describe('Api client providers', () => {
   const TEST_API_KEY = 'TEST_API_KEY'
+  const TEST_COMPANY_ID = 'c0c0c0c0-0000-4000-0000-0000000c0da7'
+  const TEST_DATACONNECTION_ID = 'dadadada-0000-4000-0000-0000000c0da7'
   let API_UNDER_TEST = null
 
   describe('api client selctor', () => {
@@ -43,7 +45,7 @@ describe('Api client providers', () => {
 
     describe('company api', () => {
       beforeEach(() => {
-        API_UNDER_TEST = API_UNDER_TEST.companyDataClient(0)
+        API_UNDER_TEST = API_UNDER_TEST.companyDataClient(TEST_COMPANY_ID)
       })
 
       it('should exist', () => {
@@ -67,13 +69,47 @@ describe('Api client providers', () => {
       })
 
       it('should have correct base url', () => {
-        API_UNDER_TEST.__baseUrl.should.be.exactly('https://api-uat.codat.io/companies/0/data')
+        API_UNDER_TEST.__baseUrl.should.be.exactly('https://api-uat.codat.io/companies/' + TEST_COMPANY_ID + '/data')
       })
 
       it('should have given api key', () => {
         API_UNDER_TEST.__apiKey.should.be.exactly(TEST_API_KEY)
       })
-    })
+    });
+
+    describe('data connection api', () => {
+      beforeEach(() => {
+        API_UNDER_TEST = API_UNDER_TEST.dataConnectionDataClient(TEST_COMPANY_ID, TEST_DATACONNECTION_ID)
+      })
+
+      it('should exist', () => {
+        should.exist(API_UNDER_TEST)
+      })
+
+      it('should contain data GET', () => {
+        API_UNDER_TEST.should.have.property('get')
+      })
+
+      it('should contain data POST', () => {
+        API_UNDER_TEST.should.have.property('post')
+      })
+
+      it('should contain data PUT', () => {
+        API_UNDER_TEST.should.have.property('put')
+      })
+
+      it('should contain data DELETE', () => {
+        API_UNDER_TEST.should.have.property('delete')
+      })
+
+      it('should have correct base url', () => {
+        API_UNDER_TEST.__baseUrl.should.be.exactly('https://api-uat.codat.io/companies/' + TEST_COMPANY_ID + '/connections/' + TEST_DATACONNECTION_ID + '/data')
+      })
+
+      it('should have given api key', () => {
+        API_UNDER_TEST.__apiKey.should.be.exactly(TEST_API_KEY)
+      })
+    });
   })
 
   describe('production', () => {
@@ -100,7 +136,7 @@ describe('Api client providers', () => {
 
     describe('company api', () => {
       beforeEach(() => {
-        API_UNDER_TEST = API_UNDER_TEST.companyDataClient(0)
+        API_UNDER_TEST = API_UNDER_TEST.companyDataClient(TEST_COMPANY_ID)
       })
 
       it('should exist', () => {
@@ -124,12 +160,46 @@ describe('Api client providers', () => {
       })
 
       it('should have correct base url', () => {
-        API_UNDER_TEST.__baseUrl.should.be.exactly('https://api.codat.io/companies/0/data')
+        API_UNDER_TEST.__baseUrl.should.be.exactly('https://api.codat.io/companies/' + TEST_COMPANY_ID + '/data')
       })
 
       it('should have given api key', () => {
         API_UNDER_TEST.__apiKey.should.be.exactly(TEST_API_KEY)
       })
-    })
+    });
+
+    describe('data connection api', () => {
+      beforeEach(() => {
+        API_UNDER_TEST = API_UNDER_TEST.dataConnectionDataClient(TEST_COMPANY_ID, TEST_DATACONNECTION_ID)
+      })
+
+      it('should exist', () => {
+        should.exist(API_UNDER_TEST)
+      })
+
+      it('should contain data GET', () => {
+        API_UNDER_TEST.should.have.property('get')
+      })
+
+      it('should contain data POST', () => {
+        API_UNDER_TEST.should.have.property('post')
+      })
+
+      it('should contain data PUT', () => {
+        API_UNDER_TEST.should.have.property('put')
+      })
+
+      it('should contain data DELETE', () => {
+        API_UNDER_TEST.should.have.property('delete')
+      })
+
+      it('should have correct base url', () => {
+        API_UNDER_TEST.__baseUrl.should.be.exactly('https://api-uat.codat.io/companies/' + TEST_COMPANY_ID + '/connections/' + TEST_DATACONNECTION_ID + '/data')
+      })
+
+      it('should have given api key', () => {
+        API_UNDER_TEST.__apiKey.should.be.exactly(TEST_API_KEY)
+      })
+    });
   })
 })
